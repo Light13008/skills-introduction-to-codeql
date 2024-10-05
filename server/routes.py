94099c2a06a3bd5eb527c7d32f989aed818b5fb0
Sarvesh.Patil <111660124+Light13008@users.noreply.github.com>
2024-10-06 03:59:25 +0530

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -1,30 +1,27 @@
 
-from flask import request, render_template, make_response
-
+from flask import request, render_template
 from server.webapp import flaskapp, cursor
 from server.models import Book
-
 
 @flaskapp.route('/')
 def index():
     name = request.args.get('name')
     author = request.args.get('author')
-    read = bool(request.args.get('read'))
+    read = request.args.get('read')  # This will be a string, convert to bool if needed
+
+    # Initialize an empty list for books
+    books = []
 
     if name:
-        cursor.execute(
-"SELECT * FROM books WHERE name LIKE %s", name        )
+        cursor.execute("SELECT * FROM books WHERE name LIKE %s", ('%' + name + '%',))
         books = [Book(*row) for row in cursor]
 
     elif author:
-        cursor.execute(
-            "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
-        "SELECT * FROM books WHERE author LIKE %s", author
-        )
+        cursor.execute("SELECT * FROM books WHERE author LIKE %s", ('%' + author + '%',))
         books = [Book(*row) for row in cursor]
 
     else:
         cursor.execute("SELECT name, author, read FROM books")
         books = [Book(*row) for row in cursor]
-        
+
     return render_template('books.html', books=books)
